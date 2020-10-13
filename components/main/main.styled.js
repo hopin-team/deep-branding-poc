e336a7599217b3fbcaed125e820ef2781e15cbe6
Ria Carmin <akimalunar@gmail.com>
2020-10-12 19:13:24 -0700

    init

---
+++ components/main/main.styled.js
@@ -0,0 +1,6 @@
+import styled from "styled-components";
+
+export const Main = styled.main`
+  max-width: 67rem;
+  margin: 0 auto;
+`;
---
+++ pages/_app.js
@@ -0,0 +1,38 @@
+import { ThemeProvider } from "styled-components";
+import { merge } from "lodash";
+import {
+  COLOR,
+  LAYOUT,
+  LINE,
+  MOTION,
+  PLANE,
+  SCALE,
+  TYPOGRAPHY,
+} from "@hopin-team/ui-symbols";
+
+export default function App({ Component, props }) {
+  return (
+    <ThemeProvider theme={props.theme ?? {}}>
+      <Component />
+    </ThemeProvider>
+  );
+}
+
+App.getInitialProps = async () => {
+  const defaultTheme = {
+    COLOR,
+    LAYOUT,
+    LINE,
+    MOTION,
+    PLANE,
+    SCALE,
+    TYPOGRAPHY,
+  };
+  const response = await fetch("https://acidic-comet-fog.glitch.me/theme");
+  const customTheme = await response.json();
+  const theme = merge(defaultTheme, customTheme);
+
+  return {
+    props: { theme },
+  };
+};
